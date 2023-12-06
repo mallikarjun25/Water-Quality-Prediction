b078f4fa08d9319049164b0f7d678a9bf8dd46ea
mallikarjun25 <68541427+mallikarjun25@users.noreply.github.com>
2023-12-06 13:39:55 -0500

    Updated file

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template, request, jsonify, url_for
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import joblib
 
-
-
-
 app = Flask(__name__)
+
+# Load the SVM model
 model = joblib.load("svm.pkl")
 
 # Load the dataset
-dataset_path = 'C:/Users/user/Desktop/Water-Quality-Prediction/water_potability.csv'
+dataset_path = '/Users/mallikarjunaitha/Documents/GitHub/Water-Quality-Prediction/water_potability.csv'
 df = pd.read_csv(dataset_path)
 
 # Define a route to get the columns
@@ -18,88 +17,34 @@ def get_columns():
     columns = list(df.columns)
     return jsonify(columns)
 
+# Define the main route
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Define the prediction route
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Fetch user inputs from the form
-    # input_data = {column: float(request.form.get(column)) for column in df.columns}
-    # Perform prediction logic (replace with your actual prediction code)
-    #prediction_result = predict_water_quality(input_data)
     if request.method == "POST":
-        ph = request.form["ph"]
-        hardness = request.form["hardness"]
-        solids = request.form["solids"]
-        chloramines = request.form["chloramines"]
-        sulfate = request.form["sulfate"]
-        conductivity = request.form["conductivity"]
-        organicCarbon = request.form["organicCarbon"]
-        trihalomethanes = request.form["trihalomethanes"]
-        turbidity = request.form["turbidity"]
+        # Fetch user inputs from the form
+        ph = float(request.form["ph"])
+        hardness = float(request.form["hardness"])
+        solids = float(request.form["solids"])
+        chloramines = float(request.form["chloramines"])
+        sulfate = float(request.form["sulfate"])
+        conductivity = float(request.form["conductivity"])
+        organicCarbon = float(request.form["organicCarbon"])
+        trihalomethanes = float(request.form["trihalomethanes"])
+        turbidity = float(request.form["turbidity"])
 
-        input_values = [ph, hardness,solids, chloramines, sulfate,conductivity, organicCarbon, trihalomethanes, turbidity]
-        input_values = [float(val) for val in input_values]
-
+        # Perform prediction
+        input_values = [ph, hardness, solids, chloramines, sulfate, conductivity, organicCarbon, trihalomethanes, turbidity]
         prediction = model.predict([input_values])
-        return jsonify({'prediction': prediction[0]})
+
+        # Return the prediction as JSON
+        return jsonify({'prediction': int(prediction[0])})
 
     return render_template('index.html')
-
-
-# Replace this function with your actual prediction logic
-#def predict_water_quality(input_data):
-    # Example: If pH is greater than 7, predict good water quality
-    #return 'Good Water Quality' if input_data['ph'] > 7 else 'Poor Water Quality'
-
-# @app.route('/ph_val',methods = ["POST"])
-# def ph_val():
-#     x = float(request.form["ph"])
-#     return render_template("result.html", x = x)
-
-# @app.route('/hard_val' ,methods = ["POST"])
-# def hard_val():
-#     hard = float(request.form["hardness"])
-#     return render_template("result.html", hard = hard)
-
-# @app.route('/solids_val' ,methods = ["POST"])
-# def solids_val():
-#     sol = float(request.form["solids"])
-#     return render_template("result.html", sol = sol)
-
-# @app.route('/chl_val' ,methods = ["POST"])
-# def chl_val():
-#     chlorn = float(request.form["chloramines"])
-#     return render_template("result.html", chlorn = chlorn)
-
-# @app.route('/sul_val' ,methods = ["POST"])
-# def sul_val():
-#     sul = float(request.form["sulfate"])
-#     return render_template("result.html", sul = sul)
-
-# @app.route('/con_val' ,methods = ["POST"])
-# def con_val():
-#     con = float(request.form["conductivity"])
-#     return render_template("result.html", con = con)
-
-# @app.route('/org_val' ,methods = ["POST"])
-# def org_val():
-#     org = float(request.form["organicCarbon"])
-#     return render_template("result.html", org = org)
-
-# @app.route('/tri_val' ,methods = ["POST"])
-# def tri_val():
-#     tri = float(request.form["trihalomethanes"])
-#     return render_template("result.html", tri = tri)
-
-# @app.route('/tur_val' ,methods = ["POST"])
-# def tur_val():
-#     tur = float(request.form["turbidity"])
-#     return render_template("result.html", tur = tur)
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,10 +1,78 @@
+// Function to check if a value is a valid number
+function isValidNumber(value) {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+}
+
+// Function to predict water quality based on user input
 function predictWaterQuality() {
-    // Fetch input values
-    const phValue = document.getElementById('ph').value;
-    // Fetch other input values
+    // Fetch input values from the form
+    var phValue = document.getElementById('ph').value;
+    var hardnessValue = document.getElementById('hardness').value;
+    var solidsValue = document.getElementById('solids').value;
+    var chloraminesValue = document.getElementById('chloramines').value;
+    var sulfateValue = document.getElementById('sulfate').value;
+    var conductivityValue = document.getElementById('conductivity').value;
+    var organicCarbonValue = document.getElementById('organicCarbon').value;
+    var trihalomethanesValue = document.getElementById('trihalomethanes').value;
+    var turbidityValue = document.getElementById('turbidity').value;
+
+    // Validate input values
+    if (!isValidNumber(phValue) || phValue < 0 || phValue > 14) {
+        // Alert the user if pH value is invalid
+        alert('Please enter a valid pH value between 0 and 14.');
+        return;
+    }
+
+    if (!isValidNumber(hardnessValue) || hardnessValue < 0) {
+        // Alert the user if hardness value is invalid
+        alert('Please enter a valid hardness value greater than or equal to 0.');
+        return;
+    }
+    if (!isValidNumber(solidsValue) || solidsValue < 0) {
+        // Alert the user if TDS value is invalid
+        alert('Please enter a valid TDS value greater than or equal to 0.');
+        return;
+    }
+
+    if (!isValidNumber(chloraminesValue) || chloraminesValue < 0) {
+        // Alert the user if value is invalid
+        alert('Please enter a valid value greater than or equal to 0.');
+        return;
+    }
+
+    if (!isValidNumber(sulfateValue) || sulfateValue < 0) {
+        // Alert the user if value is invalid
+        alert('Please enter a valid value greater than or equal to 0.');
+        return;
+    }
+
+    if (!isValidNumber(conductivityValue) || conductivityValue < 0) {
+         // Alert the user if value is invalid
+         alert('Please enter a valid value greater than or equal to 0.');
+        return;
+    }
+
+    if (!isValidNumber(organicCarbonValue) || organicCarbonValue < 0) {
+        // Alert the user if value is invalid
+        alert('Please enter a valid value greater than or equal to 0.');
+        return;
+    }
+
+    if (!isValidNumber(trihalomethanesValue) || trihalomethanesValue < 0) {
+        // Alert the user if value is invalid
+        alert('Please enter a valid value greater than or equal to 0.');
+        return;
+    }
+
+    if (!isValidNumber(turbidityValue) || turbidityValue < 0) {
+        // Alert the user if value is invalid
+        alert('Please enter a valid value greater than or equal to 0.');
+        return;
+    }
 
     // Perform prediction logic
-    const predictionResult = phValue > 7 ? 'Potable' : 'Not Potable';
+    const isPotable = phValue >= 6.5 && phValue <= 8.5;
+    const predictionResult = isPotable ? 'Potable' : 'Not Potable';
 
     // Display result
     const resultDiv = document.getElementById('result');
